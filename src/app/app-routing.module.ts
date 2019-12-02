b6f35cf0168aae4478bc7d63f4bfcf4d41bc1d57
Waylon Dixon <wdixon@waylondixon.com>
2019-12-02 16:10:00 -0700

    Routing App: create routes for departments and employees components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from './components/department-list/department-list.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'departments', component: DepartmentListComponent },
+  { path: 'employees', component: EmployeeListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
