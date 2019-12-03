909a573b2a9ff811f686fddaa107b9c4c4a2ec0e
Waylon Dixon <wdixon@waylondixon.com>
2019-12-03 12:33:13 -0700

    Routing App: setup departmentList and styling for using route parameters

--- src/app/components/department-list/department-list.component.ts
+++ src/app/components/department-list/department-list.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+    <div>
+      <h3>Department List</h3>
+      <ul class="items">
+        <li *ngFor="let department of departments">
+          <span class="badge">{{department.id}}</span> {{department.name}}
+        </li>
+      </ul>
+    </div>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
+  public departments = [
+    {"id": 1, "name":"Angular"},
+    {"id": 2, "name":"Node"},
+    {"id": 3, "name":"MongoDB"},
+    {"id": 4, "name":"Ruby"},
+    {"id": 5, "name":"Bootstrap"}
+  ];
 
   constructor() { }
 
