c4856bd7a546892d1301b3dc26a7e135bf49278f
Waylon Dixon <wdixon@waylondixon.com>
2019-12-03 11:38:06 -0700

    Routing App: wildcard and redirecting routes, added homepage and pagenotfound components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './components/department-list/department-list.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 
 const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' }, // Note: possible values for pathMatch: {prefix,full}
+  { path: '', component: HomePageComponent },
   { path: 'departments', component: DepartmentListComponent },
-  { path: 'employees', component: EmployeeListComponent }
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '**', component: PageNotFoundComponent } // Note: 'wildcard' route should be the last in the routing list
 ];
 
 @NgModule({
@@ -15,5 +20,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
   DepartmentListComponent,
-  EmployeeListComponent
+  EmployeeListComponent,
+  PageNotFoundComponent
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    PageNotFoundComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
